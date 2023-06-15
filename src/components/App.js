92686d9a73b303f80754b816f594a8b937a3fdd6
rana <trana51997@gmail.com>
2023-06-15 16:38:57 -0700

    Accordian component part 1 done

---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordian data={faqs} />
+    </div>
+  );
+}
+
+function Accordian({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <ul>
+          <AccordionItem num={i + 1} title={el.title} text={el.text} />
+        </ul>
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [showText, setShowText] = useState(false);
+
+  const showTextClassForList = showText ? "item open" : "item";
+
+  return (
+    <li
+      className={showTextClassForList}
+      onClick={() => setShowText((curState) => !curState)}
+    >
+      <p className="number">{num <= 9 ? `0${num}` : num}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{showText ? "-" : "+"}</p>
+      {showText && <p className="content-box">{text}</p>}
+    </li>
+  );
+}
